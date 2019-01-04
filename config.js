d246263cdffd08f52ee5e031acb9b41f4095b100
R-Cole <randy@goodglue.com>
2019-01-04 16:20:49 -0700

    pre user reg/login setup

--- config.js
+++ config.js
@@ -3,11 +3,9 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://robot9999:zebra999@ds145694.mlab.com:45694/goodcalldb',
-  TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://robot9999:zebra999@ds145694.mlab.com:45694/goodcalldb',
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test'
+  
   // DATABASE_URL:
   //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
   // TEST_DATABASE_URL:
