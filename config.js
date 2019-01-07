93f1c04f11d60ec4df16228cf55bec6825b0d047
R-Cole <randy@goodglue.com>
2019-01-06 17:03:32 -0700

    modified config.js for offline work

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://robot9999:zebra999@ds145694.mlab.com:45694/goodcalldb',
+  DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',//<--- switch back to mlab!
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/thinkful-backend-test'
   
   // DATABASE_URL:
@@ -12,3 +12,7 @@ module.exports = {
   //     process.env.TEST_DATABASE_URL ||
   //     'postgres://localhost/thinkful-backend-test'
 };
+
+//'mongodb://localhost/thinkful-backend' <--- no internet testing
+
+//'mongodb://robot9999:zebra999@ds145694.mlab.com:45694/goodcalldb' <--- switch back to mlab!
